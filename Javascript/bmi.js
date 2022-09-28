ea3feef80a39b87a29f13e2194edf53a39ec1f1b
ope90 <110941429+ope90@users.noreply.github.com>
2022-09-28 14:26:16 +0100

    okay

---
+++ Javascript/bmi.js
@@ -0,0 +1,13 @@
+let height = document.getElementById("height");
+let weight = document.getElementById("weight");
+
+function calculateBMI() {
+ var h = document.getElementById('h').value;
+ var w = document.getElementById('w').value;
+ var bmi = w / (h / 100 * h / 100);
+  
+
+ console.log(h, w)
+ document.getElementById("result").innerHTML = bmi
+
+}
---
+++ Javascript/script.js
@@ -0,0 +1,18 @@
+
+function turnOn() {
+ document.getElementById('bulb').src = "pic_bulb.gif"
+ console.log("turnOn activated")
+}
+
+function turnOff() {
+ document.getElementById('bulb').src = "pic_bulboff.gif"
+ console.log("turnOff activated")
+}
+
+window.alert(5 + 6)
+
+console.log("I am coming")
+
+function printPage() {
+ window.print ()
+}
