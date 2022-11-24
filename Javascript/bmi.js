6308e9280eba98478f9ecbc2ba7c3c89e7d998d9
opeyemi odunaiya <110941429+ope90@users.noreply.github.com>
2022-11-24 20:10:25 +0000

    BMI Calculator
    Bmi

--- Javascript/bmi.js
+++ Javascript/bmi.js
@@ -1,34 +1,14 @@
 let height = document.getElementById("height");
 let weight = document.getElementById("weight");
 
-function calculateBmi() {
- let h = document.getElementById('h').value;
- let w = document.getElementById('w').value;
- let bmi = w / (h / 100 * h / 100);
- let bmio = (bmi.toFixed(0));
-
-
- document.getElementById("result").innerHTML = bmio;
- 
- bmiText(bmio)
-}
-
-function bmiText(Bmi) {
+function calculateBMI() {
+ var h = document.getElementById('h').value;
+ var w = document.getElementById('w').value;
+ var bmi = w / (h / 100 * h / 100);
   
- if (Bmi < 18) {
 
-  document.getElementById('message').innerHTML =
-   'You are underweight';
-  
- } else if (Bmi >= 18.5 && Bmi <= 24.9) {
 
-  document.getElementById("message").innerHTML =
-   'You are healthy';
-  
- } else {
-   document.getElementById("message").innerHTML =
-     'You are overweight';
-
- }
+ console.log(h, w)
+ document.getElementById("result").innerHTML = bmi
 
 }
